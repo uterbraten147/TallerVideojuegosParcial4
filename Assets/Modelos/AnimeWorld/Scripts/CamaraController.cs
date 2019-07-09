1825a9e90849d4247ac7ff14dc8f9860e3b50b2e
uterbraten147 <147and@gmail.com>
2019-07-09 16:01:21 -0500

    Animacion
    movimientos y animaciones

---
+++ Assets/Modelos/AnimeWorld/Scripts/CamaraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    public float RotationSpeed = 1f;
+    public Transform target, player;
+    float mouseX, mouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        CamController();
+    }
+
+    void CamController()
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y")* RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+
+    }
+}
