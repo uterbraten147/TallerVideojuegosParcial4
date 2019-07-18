66da14b276f9f54272dc0dd2545595f3dd93023f
uterbraten147 <147and@gmail.com>
2019-07-18 18:08:13 -0500

    gun

---
+++ Assets/Modelos/AnimeWorld/Scripts/Shooting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public int Daño = 1;
+    public float fireRate = .25f;
+    public float gunRange = 50f;
+    public float hitForce = 100f;
+    public Transform trigger;
+
+    private Camera Cam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    //private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire; //siguiente disparo
+    
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        //gunAudio = GetComponent<AudioSource>();
+        Cam = GetComponentInParent<Camera>();
+        
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(disparoFX());
+            Vector3 rayOrigin = Cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, trigger.position);
+
+            if (Physics.Raycast(rayOrigin,Cam.transform.forward,out hit, gunRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+
+                laserLine.SetPosition(1, rayOrigin + (Cam.transform.forward * gunRange));
+            }
+
+        }
+        
+    }
+
+    private IEnumerator disparoFX()
+    {
+        //gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
