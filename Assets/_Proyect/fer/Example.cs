1de6a2cb08551130a05452c88790b4c9e06eca4d
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-07-19 15:00:37 -0500

    Cuernito

---
+++ Assets/_Proyect/fer/Example.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Example : MonoBehaviour
+
+{ [SerializeField]
+    public GameObject Character;
+    public GameObject Wolf;
+    public GameObject Text;
+    public GameObject target;
+    private bool Acariciar;
+    public Animator animator;
+
+
+    private void Awake()
+    {
+        Text.SetActive(false);
+        Acariciar = false ;
+
+    }
+
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        if (Input.GetKey("g"))
+        {
+
+
+            Debug.Log("Acariciado");
+            animator.SetBool("Acariciar", true);
+            Text.SetActive(true);
+            //Lookat();
+
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Text.SetActive(false);
+        Acariciar = false;
+        animator.SetBool("Acariciar", false);
+
+    }
+
+    public void Lookat()
+    {
+        Character.transform.LookAt(target.transform);
+        Acariciar = true;
+        animator.SetBool("Acariciar", true);
+    }
+}
+
