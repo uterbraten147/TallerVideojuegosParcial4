0f0375aeec3ebb2739a25e5f419dd9487ef9b977
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-07-19 21:00:24 -0500

    Xd

--- Assets/_Proyect/fer/Example.cs
+++ Assets/_Proyect/fer/Example.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 
 public class Example : MonoBehaviour
 
-{ [SerializeField]
+{ 
     public GameObject Character;
     public GameObject Wolf;
     public GameObject Text;
     public GameObject target;
+    [SerializeField]
     private bool Acariciar;
     public Animator animator;
+    public float movementSpeed = 4;
+    public bool acariciado;
 
 
     private void Awake()
     {
         Text.SetActive(false);
         Acariciar = false ;
+        acariciado = false;
 
     }
 
-    
+    public void Update()
+    {
+
+        if (acariciado)
+        {
+            transform.LookAt(Character.transform);
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+            //acariciado = false;
+        }
+        if (Input.GetKey("x"))
+        {
+            acariciado = false;
+        }
+
+
+
+    }
+
+
     private void OnCollisionStay(Collision collision)
     {
         if (Input.GetKey("g"))
@@ -29,13 +51,17 @@ public class Example : MonoBehaviour
 
 
             Debug.Log("Acariciado");
+            StartCoroutine(Esperar(30));
             animator.SetBool("Acariciar", true);
             Text.SetActive(true);
+            
+            
             //Lookat();
 
         }
 
     }
+
 
     private void OnCollisionExit(Collision collision)
     {
@@ -50,6 +76,17 @@ public class Example : MonoBehaviour
         Character.transform.LookAt(target.transform);
         Acariciar = true;
         animator.SetBool("Acariciar", true);
+    }
+
+    IEnumerator Esperar(float i)
+    {
+        Debug.Log("Entro pero aextra");
+        yield return  new WaitForSeconds(i);
+        acariciado = true;
+
+
+
+
     }
 }
 
